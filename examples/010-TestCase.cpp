d3580fc30ded289dd3326b73e352a6a134152d26
LJX <510391246@qq.com>
2026-07-08 13:45:29 +0800

    test3

--- examples/010-TestCase.cpp
+++ examples/010-TestCase.cpp
@@ -12,6 +12,8 @@ int Factorial( int number ) {
 
 TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
     REQUIRE( Factorial(0) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
 }
 
 TEST_CASE( "Factorials of 1 and higher are computed (pass)", "[single-file]" ) {
